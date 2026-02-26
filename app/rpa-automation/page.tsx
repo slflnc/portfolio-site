d1abe1bf62087fdc5429920d47cb7bed402e0935
slflnc <alfie.lancaster@outlook.com>
2026-02-26 16:15:36 +0000

    replaced and sanitised RPA video

--- app/rpa-automation/page.tsx
+++ app/rpa-automation/page.tsx
@@ -48,7 +48,7 @@ export default function RpaAutomation() {
                   poster="/poster1.png"
                   preload="metadata"
                 >
-                  <source src="/RPA.MP4" type="video/mp4" />
+                  <source src="/RPADemo.MP4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
               </div>
